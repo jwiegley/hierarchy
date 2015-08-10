af67b87febe92beff22b60ba7b987315efd06432
John Wiegley <johnw@newartisans.com>
2015-08-10 10:15:25 -0700

    Bump to 0.2.1

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@
 }:
 mkDerivation {
   pname = "hierarchy";
-  version = "0.2.0";
+  version = "0.2.1";
   src = ./.;
   libraryHaskellDepends = [
     base directory exceptions free mmorph monad-control mtl pipes
