da96ade1474a30579e80e027058ce64865e019d3
John Wiegley <johnw@newartisans.com>
2018-05-07 13:53:56 -0700

    Base the hierarchy library on logict, not pipes or streaming

--- src/Hierarchy.hs
+++ src/Hierarchy.hs
@@ -3,34 +3,35 @@
 
 module Hierarchy where
 
-import           Control.Comonad.Trans.Cofree
-import           Control.Cond
-import           Data.Functor.Of
-import           Streaming
-import qualified Streaming.Prelude as S
+import Control.Comonad.Trans.Cofree
+import Control.Cond
+import Control.Monad.Logic
 
-newtype ListT m a = ListT { runListT :: Stream (Of a) m () }
+type ListT = LogicT
 
 -- | A 'TreeT' is a tree of values, where the (possible) branches are
 -- 'ListT's.
 type TreeT m = CofreeT Maybe (ListT m)
 
+considering :: [a] -> LogicT m a
+considering xs = LogicT $ \c n -> foldr c n xs
+
 -- | Turn a generated list into a 'ListT'.
 selectEach :: Monad m => m [a] -> ListT m a
-selectEach m = ListT $ S.each =<< lift m
+selectEach = lift >=> considering
 
 -- | Descend one level into a 'TreeT', yielding a list of values and their
 -- possible associated trees.
 descend :: Monad m => TreeT m a -> ListT m (a, Maybe (TreeT m a))
-descend (CofreeT (ListT t)) = ListT $ S.for t $ \(a :< mp) -> S.yield (a, mp)
+descend (CofreeT t) = t >>- \(a :< mp) -> pure (a, mp)
 {-# INLINE descend #-}
 
 -- | Perform a depth-first traversal of a 'TreeT', yielding a 'ListT' of its
 -- contents. Note that breadth-first traversals cannot offer static memory
 -- guarantees, so they are not provided by this module.
 walk :: Monad m => TreeT m a -> ListT m a
-walk (CofreeT (ListT t)) = ListT $ S.for t $ \(a :< mp) ->
-    S.yield a >> maybe (return ()) (runListT . walk) mp
+walk (CofreeT t) = t >>- \(a :< (mp :: Maybe (TreeT m a))) ->
+    LogicT $ \c n -> c a (maybe n (\p -> unLogicT (walk p) c n) mp)
 {-# INLINEABLE walk #-}
 
 -- | Given a 'TreeT', produce another 'TreeT' which yields only those elements
@@ -52,9 +53,9 @@ walk (CofreeT (ListT t)) = ListT $ S.for t $ \(a :< mp) ->
 --     runEffect $ for (runListT (walk files)) $ liftIO . print
 -- @
 winnow :: Monad m => TreeT m a -> CondT a m () -> TreeT m a
-winnow (CofreeT (ListT t)) p = CofreeT $ ListT $ S.for t $ \(a :< mst) -> do
+winnow (CofreeT t) p = CofreeT $ t >>- \(a :< mst) -> do
     (mval, mnext) <- lift $ execCondT a p
     let mnext' = winnow <$> mst <*> mnext
     case mval of
-        Nothing -> maybe (return ()) (runListT . runCofreeT) mnext'
-        Just a' -> S.yield (a' :< mnext')
+        Nothing -> maybe mzero runCofreeT mnext'
+        Just a' -> pure (a' :< mnext')
