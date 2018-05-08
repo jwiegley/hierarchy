6579bb9f501c11887c9a85c847a05749a9e67c1a
John Wiegley <johnw@newartisans.com>
2018-05-08 12:16:36 -0700

    Thanks to a blog post from Gabriel, we don't even need LogicT
    http://www.haskellforall.com/2014/11/how-to-build-library-agnostic-streaming.html

--- src/Hierarchy.hs
+++ src/Hierarchy.hs
@@ -3,59 +3,56 @@
 
 module Hierarchy where
 
+import Control.Monad
 import Control.Comonad.Trans.Cofree
 import Control.Cond
-import Control.Monad.Logic
-
-type ListT = LogicT
 
 -- | A 'TreeT' is a tree of values, where the (possible) branches are
--- 'ListT's.
-type TreeT m = CofreeT Maybe (ListT m)
+--   'ListT's over some Monad 'm'.
+type TreeT m = CofreeT Maybe m
 
-considering :: [a] -> LogicT m a
-considering xs = LogicT $ \c n -> foldr c n xs
-
--- | Turn a generated list into a 'ListT'.
-selectEach :: Monad m => m [a] -> ListT m a
-selectEach = lift >=> considering
+-- | Turn a list into a series of possibilities:
+--
+-- >>> observeAllT $ considering [1,2,3]
+-- [1,2,3]
+select :: MonadPlus m => [a] -> m a
+select = msum . map pure
 
 -- | Descend one level into a 'TreeT', yielding a list of values and their
--- possible associated trees.
-descend :: Monad m => TreeT m a -> ListT m (a, Maybe (TreeT m a))
-descend (CofreeT t) = t >>- \(a :< mp) -> pure (a, mp)
+--   possible associated trees.
+descend :: MonadPlus m => TreeT m a -> m (a, Maybe (TreeT m a))
+descend (CofreeT t) = t >>= \(a :< mp) -> pure (a, mp)
 {-# INLINE descend #-}
 
 -- | Perform a depth-first traversal of a 'TreeT', yielding a 'ListT' of its
--- contents. Note that breadth-first traversals cannot offer static memory
--- guarantees, so they are not provided by this module.
-walk :: Monad m => TreeT m a -> ListT m a
-walk (CofreeT t) = t >>- \(a :< (mp :: Maybe (TreeT m a))) ->
-    LogicT $ \c n -> c a (maybe n (\p -> unLogicT (walk p) c n) mp)
+--   contents. Note that breadth-first traversals cannot offer static memory
+--   guarantees, so they are not provided by this module.
+walk :: MonadPlus m => TreeT m a -> m a
+walk (CofreeT t) = t >>= \(a :< mp) -> pure a `mplus` maybe mzero walk mp
 {-# INLINEABLE walk #-}
 
 -- | Given a 'TreeT', produce another 'TreeT' which yields only those elements
--- (and sub-trees) matching the given monadic conditional. This conditional
--- (see 'Control.Cond.CondT') can choose both elements and points of
--- recursion, making it capable of expressing any tree traversal in the form
--- of a predicate DSL. This differs from an expression-based traversal, like
--- XPath or Lens, in that effects in 'm' may be used to guide selection.
+--   (and sub-trees) matching the given monadic conditional. This conditional
+--   (see 'Control.Cond.CondT') can choose both elements and points of
+--   recursion, making it capable of expressing any tree traversal in the form
+--   of a predicate DSL. This differs from an expression-based traversal, like
+--   XPath or Lens, in that effects in 'm' may be used to guide selection.
 --
--- For example, to print all Haskell files under the current directory:
+--   For example, to print all Haskell files under the current directory:
 --
 -- @
---     let files = winnow (directoryFiles ".") $ do
---             path <- query
---             liftIO $ putStrLn $ "Considering " ++ path
---             when (path @`elem@` [".@/@.git", ".@/@dist", ".@/@result"])
---                 prune  -- ignore these, and don't recurse into them
---             guard_ (".hs" @`isInfixOf@`)  -- implicitly references 'path'
---     runEffect $ for (runListT (walk files)) $ liftIO . print
+-- let files = winnow (directoryFiles ".") $ do
+--         path <- query
+--         liftIO $ putStrLn $ "Considering " ++ path
+--         when (path @`elem@` [".@/@.git", ".@/@dist", ".@/@result"])
+--             prune  -- ignore these, and don't recurse into them
+--         guard_ (".hs" @`isInfixOf@`)  -- implicitly references 'path'
+-- runEffect $ for (runListT (walk files)) $ liftIO . print
 -- @
-winnow :: Monad m => TreeT m a -> CondT a m () -> TreeT m a
-winnow (CofreeT t) p = CofreeT $ t >>- \(a :< mst) -> do
-    (mval, mnext) <- lift $ execCondT a p
+winnow :: MonadPlus m => TreeT m a -> CondT a m () -> TreeT m a
+winnow (CofreeT t) p = CofreeT $ t >>= \(a :< mst) -> do
+    (mval, mnext) <- execCondT a p
     let mnext' = winnow <$> mst <*> mnext
     case mval of
         Nothing -> maybe mzero runCofreeT mnext'
-        Just a' -> pure (a' :< mnext')
+        Just a' -> pure $ a' :< mnext'
